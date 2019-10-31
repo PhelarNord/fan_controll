3a101b1c892b3248c9cac0ae483f58a461a59c0d
PhelarNord <patrik_nord@hotmail.com>
2019-10-31 07:05:02 +0100

    Started with a fanSpeed function

--- fanControll.js
+++ fanControll.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
+const gpio = require('pigpio');
+const fan = new gpio(10, {mode: gpio.OUTPUT});
 
 var temp = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp');
 var temp_c = temp/1000;
+
+fanSpeed(){
+    if(temp_c<=60){
+
+    }
+    else if(){
+        
+    }
+}
 
 console.log(temp_c);
