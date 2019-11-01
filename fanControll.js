548727d90f10a0a672813db96ce18f62b97e90e8
PhelarNord <patrik_nord@hotmail.com>
2019-11-01 07:14:27 +0100

    Some changes

--- fanControll.js
+++ fanControll.js
@@ -4,14 +4,23 @@ const fan = new gpio(10, {mode: gpio.OUTPUT});
 
 var temp = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp');
 var temp_c = temp/1000;
+const pulseWidthLow = 15;
+const pulseWidthHigh = 25;
+
 
 fanSpeed(){
     if(temp_c<=60){
 
+        fan.servoWrite(pulseWidthLow);
+        
+
     }
     else if(){
+
+        fan.servoWrite(pulseWidthHigh)
         
     }
 }
+setTimeout(fanSpeed,100000);
 
 console.log(temp_c);
