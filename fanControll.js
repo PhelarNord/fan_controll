34bda05dc26f44e4b949a4a00390a93002bbb32e
PhelarNord <patrik_nord@hotmail.com>
2019-11-03 16:35:26 +0100

    s

--- fanControll.js
+++ fanControll.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
 const Gpio = require('pigpio').Gpio;
-const fan = new gpio(10, {mode: Gpio.OUTPUT});
+const fan = new Gpio(10, {mode: Gpio.OUTPUT});
 
 var temp = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp');
 var temp_c = temp/1000;
-const pulseWidthLow = 1000;
-const pulseWidthHigh = 2000;
+const high = 255;
+const low = 100;
 
-setTimeout(fanSpeed,100000);
+setInterval(fanSpeed,5000);
 
 function fanSpeed();{
     if(temp_c<=60){
 
-        fan.servoWrite(pulseWidthLow);
+        fan.pwmWrite(low);
         console.log('Low',temp_c);
         
 
     }
     else{
 
-        fan.servoWrite(pulseWidthHigh);
+        fan.pwmWrite(high);
         console.log('High',temp_c);
         
     }
